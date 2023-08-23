fcc55304199476779153cf9b34982be8ee5fbc53
TreySchneider95 <tschneid95@gmail.com>
2023-08-23 08:55:50 -0700

    changed pacage.json for deploy

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/socks', sockRouter)
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/socks', sockRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- front_end/src/lib/Axios.js
+++ front_end/src/lib/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Axios = axios.create({
-    baseURL: "http://localhost:8000/"
+    baseURL: process.env.REACT_APP_AXIOS === 'production' ? '/api' :  "http://localhost:8000/api"
 })
 
 export default Axios
